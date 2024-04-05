decb178a1e5ebe3d92cba9191c45b7a8a61853ae
João Mota <110414160+jm-joaomota@users.noreply.github.com>
2024-04-05 15:02:07 +0100

    Update meal_time.py

--- week1/meal_time.py
+++ week1/meal_time.py
@@ -1,1 +1,21 @@
+def main():
+    time = input("What time is it: ").strip()
+    time = convert(time)
+    if 7.0 <= time <= 8.0:
+        print("breakfast time")
+    elif 12.0 <= time <= 13.0:
+        print("launch time")
+    elif 18.0 <= time <= 19.0:
+        print("dinner time")
+    else:
+        return
+
+def convert(time):
+    hours, minutes = time.split(":")
+    hours = float(hours) + (float(minutes) / 60)
+    return hours
+
+
+if __name__ == "__main__":
+    main()
 
