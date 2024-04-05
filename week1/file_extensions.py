f11a9a1cb331b2f4b8e864daf510a8a924d3904a
João Mota <110414160+jm-joaomota@users.noreply.github.com>
2024-04-05 15:01:07 +0100

    Update file_extensions.py

--- week1/file_extensions.py
+++ week1/file_extensions.py
@@ -1,1 +1,16 @@
+#Ask user for a file name
+file = input("File Name: ").lower().strip().split(".")
+
+application = ["zip", "pdf"]
+image = ["png", "gif", "jpeg", "jpg", "png"]
+text = ["txt"]
+
+if file[-1] in text:
+    print("text/plain")
+elif file[-1] in application:
+    print(f"application/{file[-1]}")
+elif file[-1] in image:
+    print(f"image/{file[-1]}")
+else:
+    print("application/octet-stream")
 
