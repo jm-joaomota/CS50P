273a401b7e4cb460c3632ae15243a51af240b2bd
João Mota <110414160+jm-joaomota@users.noreply.github.com>
2024-04-05 15:00:41 +0100

    Update deep_thought.py

--- week1/deep_thought.py
+++ week1/deep_thought.py
@@ -1,1 +1,8 @@
+user_input = input("What is the answer to the Great Question of Life, the Universe and Everything? ").lower().strip()
+
+
+if user_input == ("42") or user_input == ("forty-two") or user_input == ("forty two"):
+    print("Yes")
+else:
+    print("No")
 
