24a1d9cdd7983bfd13c8b53599120a4ebb77c59c
João Mota <110414160+jm-joaomota@users.noreply.github.com>
2024-04-05 15:01:25 +0100

    Update home_federal_savings_bank.py

--- week1/home_federal_savings_bank.py
+++ week1/home_federal_savings_bank.py
@@ -1,1 +1,11 @@
+#Ask user for a greeting
+
+greet = str(input("Geeting: ")).lower().strip()
+
+if greet.startswith("hello"):
+    print("0")
+elif greet.startswith("h"):
+    print("20")
+else:
+    print("100")
 
