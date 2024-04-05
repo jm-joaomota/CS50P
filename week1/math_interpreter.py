7beb6ba5fb730005a1298c50c086a59e50c625b2
João Mota <110414160+jm-joaomota@users.noreply.github.com>
2024-04-05 15:01:42 +0100

    Update math_interpreter.py

--- week1/math_interpreter.py
+++ week1/math_interpreter.py
@@ -1,1 +1,26 @@
+#Ask user for an input
+math = input("Math: ").strip()
+
+#Splits the input string into three parts based on the space character
+x, y, z = math.split(" ")
+
+#Converts x and y in float numbers
+num1 = float(x)
+num2 = float(z)
+
+#Do the maths.
+#.1f control the precision of the output
+
+if y == "+":
+    result = num1+num2
+    print(f"{result:.1f}")
+elif y == "-":
+    result = num1-num2
+    print(f"{result:.1f}")
+elif y == "*":
+    result = num1*num2
+    print(f"{result:.1f}")
+elif y == "/":
+    result = num1/num2
+    print(f"{result:.1f}")
 
