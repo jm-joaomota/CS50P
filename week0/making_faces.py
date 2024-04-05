390b63c1337f43f25cd8bee5c32b60147df6770c
João Mota <110414160+jm-joaomota@users.noreply.github.com>
2024-04-05 14:59:33 +0100

    Update making_faces.py

--- week0/making_faces.py
+++ week0/making_faces.py
@@ -1,1 +1,14 @@
+#Take input from user.
+
+def main():
+    message = input("Message")
+
+    print(convert(message))
+
+#Create a function that converts emoticons in emojis
+
+def convert(text):
+    return(text.replace(":)", "🙂").replace(":(", '🙁' ))
+
+main()
 
