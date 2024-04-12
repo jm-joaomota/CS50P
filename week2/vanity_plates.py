2913acd5e3230df7c9ac8a694b61ce94be05d605
João <110414160+joaomanuelmota@users.noreply.github.com>
2024-04-12 12:28:46 +0100

    Update vanity_plates.py

--- week2/vanity_plates.py
+++ week2/vanity_plates.py
@@ -1,1 +1,38 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+#check if the plate starts with 2 letters
+def check_start(plate):
+    if plate[0:2].isalpha():
+        return True
+    return False
+
+#check if the plate has between 2 and six alphanumuerics letters
+
+def check_lenght(plate):
+    if 2 <= len(plate) <= 6 and plate.isalnum():
+        return True
+    return False
+
+def check_numbers(plate):
+    for index, _ in enumerate(plate):
+        if plate[index].isdigit():
+            if plate[index] == "0" or not plate[index:].isdigit():
+                return False
+            break
+    return True
+
+
+def is_valid(s):
+    if check_start(s) and check_lenght(s) and check_numbers(s):
+        return True
+    return False
+
+
+
+main()
 
