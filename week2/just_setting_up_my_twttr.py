59ba398803fe77a057064a1102247c105df72dba
João <110414160+joaomanuelmota@users.noreply.github.com>
2024-04-12 11:23:23 +0100

    Update just_setting_up_my_twttr.py

--- week2/just_setting_up_my_twttr.py
+++ week2/just_setting_up_my_twttr.py
@@ -1,1 +1,9 @@
+user_input = input("Input: ")
+
+vowels = "AEIOUaeiou"
+
+for vowel in vowels:
+    if vowel in vowels:
+        user_input = user_input.replace(vowel, "")
+print(f"Output: {user_input}")
 
