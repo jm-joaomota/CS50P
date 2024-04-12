bdc723717e0871b06da8ffdca9b8d4be801abc6d
João <110414160+joaomanuelmota@users.noreply.github.com>
2024-04-12 10:54:20 +0100

    Update coke_machine.py

--- week2/coke_machine.py
+++ week2/coke_machine.py
@@ -1,1 +1,15 @@
 
+price = 50
+amount = 0
+coins = [5, 10, 25]
+
+while amount < price:
+    print(f"Amount Due: {price - amount}")
+
+#get input for the user
+    user_input = int(input("Insert Coin: "))
+    if user_input in coins:
+        amount += user_input
+
+print(f"Change Owed: {amount - price}")
+
