d7ae2e4d964bbf2094407d422495064436504605
João <110414160+joaomanuelmota@users.noreply.github.com>
2024-04-29 11:00:58 +0100

    Create test_plates.py

---
+++ week5/tes_plates/test_plates.py
@@ -0,0 +1,11 @@
+from plates import is_valid
+import pytest
+
+
+def test_isvalid():
+    assert is_valid("CS50") == True
+    assert is_valid("CS05") == False
+    assert is_valid("CS50P") == False
+    assert is_valid("PI3.14") == False
+    assert is_valid("H") == False
+    assert is_valid("H") == False
